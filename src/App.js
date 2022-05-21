b751533eb319fc9ad3d855d31b5666a4e855d8d3
Hossain-Md-Maruf <marufhossain220195@gmail.com>
2022-05-21 11:01:37 +0600

    css added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { toBePartiallyChecked } from '@testing-library/jest-dom/dist/matchers';
 import React, { Component } from 'react';
 import './App.css';
 import Book from './components/Book';
@@ -37,23 +38,50 @@ class App extends Component
   
   render()
   {
-    console.log(this.state);
+    const style ={
+      border: "1px solid red",
+      borderRadius: "5px",
+      backgroundColor: "black",
+      color: "wheat"
+    }
+    const buttonStyle=
+    {
+      height: "35px",
+      width: "80px",
+      border: "1px solid red",
+      padding: "2px",
+      backgroundColor:"black",
+      color:"wheat",
+      borderRadius: "5px",
+      
+      marginBottom: "2px",
+    }
+    const inputStyle=
+    {
+      height: "35px",
+      width: "230px",
+      border: "1px solid yellow",
+      backgroundColor:"cyan",
+      color:"white",
+      borderRadius: "5px",
+     
+      
+    }
+    const books = this.state.books.map(book=>
+    {
+      return(<Book 
+        bookName ={book.bookName}
+        writer ={book.writer}
+        />);
+     
+    });
+    console.log(books);
     return(
       <div className="App">
-        <h1>Book List</h1>
-        <button onClick={()=>this.changeBookState("Nineten Eighty-four")}>Change State</button>
-        <input type="text" onChange = {this.changeWithInputState} />
-        <Book 
-        bookName={this.state.books[0].bookName}
-         writer={this.state.books[0].writer}
-         inputName = {this.changeWithInputState} />
-        <Book 
-        bookName={this.state.books[1].bookName} 
-        writer={this.state.books[1].writer}/>
-        <Book 
-        bookName={this.state.books[2].bookName} 
-        writer={this.state.books[2].writer}
-        change = {()=>this.changeBookState("19090")} />
+        <h1 style={style}>Book List</h1>
+        <button style={buttonStyle} onClick={()=>this.changeBookState("Nineten Eighty-four")}>Change State</button>
+        <input style={inputStyle} type="text" onChange = {this.changeWithInputState} />
+      {books}
       </div>
     );
   }
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,11 @@
 import React from "react";
+import '../stylesheets/Book.css';
+import '../App'
 
 const Book = props =>
 {
     return(
-    <div>
+    <div className="Book">
         <h2 onClick={props.change}>Book: {props.bookName}</h2>
         <h3>Writer: {props.writer}</h3>
         <input type = 'text' onChange = {props.inputName} value = {props.bookName}/>
